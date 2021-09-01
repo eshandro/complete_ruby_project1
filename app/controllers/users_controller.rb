e0b4def881ceaa1b83b5c7f94a6209fd549cdd1e
eshandro <erik843@gmail.com>
2021-09-01 07:28:35 -0600

    Adds user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # GET /users/1
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  # GET /users
+  def index
+    @users = User.all
+  end
+
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
@@ -14,7 +25,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "#{@user.username}! You have successfully updated your profile."
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
